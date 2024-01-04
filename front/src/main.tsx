59192c71eaabf4a51b97e9315feb8e5faa914703
kuniaki <web.t.c.o.k@gmail.com>
2024-01-04 21:08:50 +0900

    no message

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import App from './App.jsx'
 import './index.css'
 
+/* !（非nullアサーション演算子）その左側の式がnullまたはundefinedでないことをコンパイラに伝えるもの */
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
