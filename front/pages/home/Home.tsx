df38c42227d940fca3f13c85413062c025dbc504
kuniaki <web.t.c.o.k@gmail.com>
2024-01-08 21:41:53 +0900

    一旦整理

--- front/pages/home/Home.tsx
+++ front/pages/home/Home.tsx
@@ -1,14 +1,10 @@
-import Header from "../../src/components/header/Header";
-import Body from "../../src/components/body/Body";
-import Footer from "../../src/components/footer/Footer";
+
 
 const Home = () => {
     return (
         <div id="HOME">
             <div className="homeConteiner">
-                <Header />
-                <Body />
-                <Footer />
+                HOME
             </div>
         </div>
     )
--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,11 +1,11 @@
 import Home from '../pages/home/Home';
+import './resset.css';
 // import Template from '../template/Template';
 
 const App = () => {
   return (
     <>
       <Home />
-      {/* <Template /> */}
     </>
   )
 
