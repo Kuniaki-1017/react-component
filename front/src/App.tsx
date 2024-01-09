cccaba0445da1391c83b8e031e439ab7fc6d7a19
kuniaki <web.t.c.o.k@gmail.com>
2024-01-10 02:06:37 +0900

    todoリスト完了

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,6 +1,6 @@
 import Home from '../pages/home/Home';
 import Comp from '../pages/comp/Comp';
-import Todo from '../pages/todo/Todo';
+
 import './resset.css';
 /* ルーティング処理用のモジュール読込 */
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
@@ -13,7 +13,6 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/comp" element={<Comp />} />
-        <Route path="/todo" element={<Todo />} />
       </Routes>
     </Router>
 
