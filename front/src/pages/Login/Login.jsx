aa82cf9798f519dbdfad4626e2ea54f6b4385cd7
kuniaki <web.t.c.o.k@gmail.com>
2024-02-10 23:36:37 +0900

    可変vw関数定義

--- front/src/pages/Login/Login.jsx
+++ front/src/pages/Login/Login.jsx
@@ -1,17 +1,43 @@
 import styled from 'styled-components';
 
-const Login = () => {
+
+    const base =  750;
+    const baseP = 1200;
+
+    // VW
+    function gv(number) {
+        /*baseを100%ととし、与えられた値の割合を算出し%に直し単位をvwにして返す関数 */
+        /*画面幅100%に対して 1vwは1%である。 ということは画面幅100%の内1%=1vwになるので割合%をvwにすると画面幅に応じて可変させてることができる  */
+        return ((number / base )* 100 * 1)+"vw";
+    }
+
+    function gvP(number) {
+    return ((number / baseP )* 100 * 1)+"vw";
+    }
+
+    gvP();
+    gv();
+
+    console.log('gvP',gvP(100));
+    console.log(1);
+
+    // スタイル////
     const LoginWrap = styled.div`
-    width
-    
+    `;
+
+    const Header = styled.header`
+        background-color: #1877F2;
+        font-size:${gvP(30)};
+
     `;
     
+const Login = () => {
     
     return(
         <>
-        <header className="header">
+        <Header className="header">
             <div className="header__in"><a href="#" className="header__logo">Knouw How</a></div>
-        </header>
+        </Header>
         <LoginWrap>
             <div className="login__in">
                 <div className="login__right">
@@ -23,8 +49,8 @@ const Login = () => {
                         <form className="login__form">
                             <input type="text" className="login__mail" placeholder='メールアドレスを入力' />
                             <input type="text" className="login__password" placeholder='パスワードを入力' />
-                            <submit className="login__submit" type="submit">ログイン</submit>
-                            <a href="#" className="login__registerLink">アカウント作成</a>
+                            <input className="login__submit" type="submit" value="ログイン"/>
+                            <a href="#" className="login__regiPsterLink">アカウント作成</a>
                         </form>
                     </div>
                 </div>
