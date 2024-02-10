e1319e11b239a91c51afb5c48429e56a8537f31f
kuniaki <web.t.c.o.k@gmail.com>
2024-02-10 22:13:54 +0900

    vscoeファイル作成しsettingjson設定

--- front/src/pages/Login/Login.jsx
+++ front/src/pages/Login/Login.jsx
@@ -3,11 +3,16 @@ import styled from 'styled-components';
 const Login = () => {
     const Title = styled.h1`
      color:red;
+     font-size:50px;
+     
+    
     `;
+    
     
     return(
         <div className="Login">
             <Title>ログインページ</Title>
+            .d
         </div>
     )
 }
