d08b213e6bd6a65451cd7db358299bd6388458bd
kuniaki <web.t.c.o.k@gmail.com>
2024-02-10 22:52:21 +0900

    vscode-styled-components拡張機能インストール

--- front/src/pages/Login/Login.jsx
+++ front/src/pages/Login/Login.jsx
@@ -1,19 +1,36 @@
 import styled from 'styled-components';
 
 const Login = () => {
-    const Title = styled.h1`
-     color:red;
-     font-size:50px;
-     
+    const LoginWrap = styled.div`
+    width
     
     `;
     
     
     return(
-        <div className="Login">
-            <Title>ログインページ</Title>
-            .d
-        </div>
+        <>
+        <header className="header">
+            <div className="header__in"><a href="#" className="header__logo">Knouw How</a></div>
+        </header>
+        <LoginWrap>
+            <div className="login__in">
+                <div className="login__right">
+                <h1 className="login__ttl">Know How</h1>
+                <p className="login__txt">ノウハウを資産化し、チームの力を最大限に引き出す。</p>
+                </div>
+                <div className="login__left">
+                    <div className="login__inputArea">
+                        <form className="login__form">
+                            <input type="text" className="login__mail" placeholder='メールアドレスを入力' />
+                            <input type="text" className="login__password" placeholder='パスワードを入力' />
+                            <submit className="login__submit" type="submit">ログイン</submit>
+                            <a href="#" className="login__registerLink">アカウント作成</a>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </LoginWrap>
+        </>
     )
 }
 
