53634a94479233ec6c2f5c9df667fd6c8e13aad4
kuniaki <web.t.c.o.k@gmail.com>
2024-02-12 08:34:45 +0900

    ヘッダーコンポーネント完了

---
+++ front/src/components/Header.jsx
@@ -0,0 +1,68 @@
+import styled from 'styled-components';
+import {gvP,gvB,gv,mediaP, mediaB, media } from '../mixin';
+
+const HeaderWrap = styled.header`
+    background-color: #1877F2;
+    height: 80px;
+    line-height: 80px;
+    
+    ${mediaP(`
+    height: ${gvP(80)};
+    line-height:  ${gvP(80)};
+    font-size:${gvP(36)};
+    `)}
+    ${mediaB(`
+    height: ${gvB(80)};
+    line-height:  ${gvB(80)};
+    `)}
+    ${media(`
+    height: ${gv(60)};
+    line-height:  ${gv(60)};
+    `)}
+
+
+
+    .header{ 
+        &__in{
+            padding: 0 30px;
+            ${mediaP(`
+            padding:0 ${gvP(30)};
+            `)}
+            ${mediaB(`
+                padding:0 ${gvB(30)};
+            `)}
+            ${media(`
+                padding:0 ${gv(20)};
+            `)}
+        }
+
+        &__logo{
+            font-size: 3.4rem;
+            color:#fff;
+            font-weight:bold;
+            ${mediaP(`
+                font-size: 3.4rem;
+            `)}
+            ${mediaB(`
+                font-size:3rem;
+            `)}
+            ${media(`
+                font-size: 2.4rem;
+            `)}
+        }
+    }
+`;
+
+
+
+const Header = () => {
+    return (
+        <HeaderWrap className="header">
+        <div className="header__in"><a href="#" className="header__logo">Knouw How</a></div>
+        </HeaderWrap>
+    )
+}
+
+export default Header;
+
+
--- front/src/pages/Login/Login.jsx
+++ front/src/pages/Login/Login.jsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 /* mixinを定義したjsの読込 */
 import {gvP,gvB,gv,mediaP, mediaB, media } from '../../mixin';
+import Header from '../../components/Header';
 
 
 
@@ -42,7 +43,7 @@ import {gvP,gvB,gv,mediaP, mediaB, media } from '../../mixin';
                 width: 80%;
             `)}
             ${media(`
-                 width: 100%;
+                width: 100%;
             `)}
         }
         &__ttl{
@@ -190,57 +191,6 @@ import {gvP,gvB,gv,mediaP, mediaB, media } from '../../mixin';
     }`;
 
 
-    const Header = styled.header`
-    background-color: #1877F2;
-    height: 80px;
-    line-height: 80px;
-    
-    ${mediaP(`
-    height: ${gvP(80)};
-    line-height:  ${gvP(80)};
-    font-size:${gvP(36)};
-    `)}
-    ${mediaB(`
-    height: ${gvB(80)};
-    line-height:  ${gvB(80)};
-    `)}
-    ${media(`
-    height: ${gv(60)};
-    line-height:  ${gv(60)};
-    `)}
-
-
-
-    .header{ 
-        &__in{
-            padding: 0 30px;
-            ${mediaP(`
-            padding:0 ${gvP(30)};
-            `)}
-            ${mediaB(`
-                padding:0 ${gvB(30)};
-            `)}
-            ${media(`
-                padding:0 ${gv(20)};
-            `)}
-        }
-
-        &__logo{
-            font-size: 3.4rem;
-            color:#fff;
-            font-weight:bold;
-            ${mediaP(`
-                font-size: 3.4rem;
-            `)}
-            ${mediaB(`
-                font-size:3rem;
-            `)}
-            ${media(`
-                font-size: 2.4rem;
-            `)}
-        }
-    }`;
-
     const Footer = styled.footer`
     background-color: #1877F2;
     height: 80px;
@@ -294,9 +244,7 @@ const Login = () => {
     
     return(
         <>
-        <Header className="header">
-            <div className="header__in"><a href="#" className="header__logo">Knouw How</a></div>
-        </Header>
+        <Header/>
         <LoginWrap className="login">
             <div className="login__in">
                 <div className="login__left">
