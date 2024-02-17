48949f8d788af2b97961b01ac6b72644ec8fb42e
kuniaki <web.t.c.o.k@gmail.com>
2024-02-18 02:48:29 +0900

    File Saver.min.js作成

--- front/trimmer/main.js
+++ front/trimmer/main.js
@@ -9,76 +9,71 @@
 
 //=====選択した画像をTrim begforeに描画=====//
 
-const before = document.querySelector('.js-before');
-const imgFile = document.querySelector('.js-imgFile');
-//canvas要素を取得
-const canvasEl = document.querySelector('.js-canvas');
-//canvasのコンテキストを用意
-const ctx = canvasEl.getContext('2d');
+// const before = document.querySelector('.js-before');
+// const imgFile = document.querySelector('.js-imgFile');
+// //canvas要素を取得
+// const canvasEl = document.querySelector('.js-canvas');
+// //canvasのコンテキストを用意
+// const ctx = canvasEl.getContext('2d');
 
-//imgeオブジェクトを生成
-const image = new Image();
-const cvsWidth = 800;
-const cvsHeight = 800;
+// //imgeオブジェクトを生成
+// const image = new Image();
+// const cvsWidth = 800;
+// const cvsHeight = 800;
 
-canvasEl.width = cvsWidth;
-canvasEl.height = cvsHeight;
-
+// canvasEl.width = cvsWidth;
+// canvasEl.height = cvsHeight;
 
 
 //loadLocalImg:ローカルの画像を読込む関数を定義
 //引数に選択されたファイルオブジェクトが入る
-function loadLocalImg(e) {
-    //①こちらの関数の外でinput[file]がchangeしたときのイベント処理を記述しコールバックでこちらの関数を渡す
-    //②input[file]にて選択されたファイルの取得
-    //③選択されたファイルが正しい形式かチェック
-    //④ファイルを読み込むためのFILE APIをインスタンス化
-    //⑤選択されたファイルをインスタンス名.readAsDataURLで読み込む※読込結果はインスタンス名.resultに格納される
-    //⑥File Apiで読み込んだファイルを任意のイベントで処理をする。ここではonload(読み込んだら)したら各処理を行う
-    //⑦ブラウザがFile APIに対応しているかチェックする関数を作成し、その中でinput[file]のchangeイベントを発火させる
+// function loadLocalImg(e) {
+//     //①こちらの関数の外でinput[file]がchangeしたときのイベント処理を記述しコールバックでこちらの関数を渡す
+//     //②input[file]にて選択されたファイルの取得
+//     //③選択されたファイルが正しい形式かチェック
+//     //④ファイルを読み込むためのFILE APIをインスタンス化
+//     //⑤選択されたファイルをインスタンス名.readAsDataURLで読み込む※読込結果はインスタンス名.resultに格納される
+//     //⑥File Apiで読み込んだファイルを任意のイベントで処理をする。ここではonload(読み込んだら)したら各処理を行う
+//     //⑦ブラウザがFile APIに対応しているかチェックする関数を作成し、その中でinput[file]のchangeイベントを発火させる
 
 
-    //引数に渡されたファイル情報を取得
-    //e.target/files[0]の中にimgの情報が入っている
-    let fileData = e.target.files[0];
-    console.log(fileData.type);
+//     //引数に渡されたファイル情報を取得
+//     //e.target/files[0]の中にimgの情報が入っている
+//     let fileData = e.target.files[0];
+//     console.log(fileData.type);
 
-    //画像ファイル以外はエラーを出す
-    //fileData.type→ファイルのMIMETYPEを返す。match('image.*')は選択されたファイルタイプがimageで始まるか判定する
-    if (!fileData.type.match('image.*')) {
-        alert('画像ファイルを選択してください');
-        return;
-    }
+//     //画像ファイル以外はエラーを出す
+//     //fileData.type→ファイルのMIMETYPEを返す。match('image.*')は選択されたファイルタイプがimageで始まるか判定する
+//     if (!fileData.type.match('image.*')) {
+//         alert('画像ファイルを選択してください');
+//         return;
+//     }
 
-    //FileReaderオブジェクトを使ってファイルを読み込み
-    const reader = new FileReader();
-    //ファイル読込を実行
-    reader.readAsDataURL(fileData);
-    //インスタンス名.reaadAs~が実行されると下記.onladが実行されresult等の値が取得できる
-    //onloadの他にエラーが発生した時用の.onerrorなどもある
-    reader.onload = function () {
+//     //FileReaderオブジェクトを使ってファイルを読み込み
+//     const reader = new FileReader();
+//     //ファイル読込を実行
+//     reader.readAsDataURL(fileData);
+//     //インスタンス名.reaadAs~が実行されると下記.onladが実行されresult等の値が取得できる
+//     //onloadの他にエラーが発生した時用の.onerrorなどもある
+//     reader.onload = function () {
 
-        //読み込んだファイル表示用のimgタグを生成
-        let img = document.createElement('img');
-        //imgタグのsrcに画像データを設定
-        img.src = reader.result;
-        //上記imgタグをbeforeの子要素に挿入
-        before.appendChild(img);
-    }
-}
-
-
+//         //読み込んだファイル表示用のimgタグを生成
+//         let img = document.createElement('img');
+//         //imgタグのsrcに画像データを設定
+//         img.src = reader.result;
+//         //上記imgタグをbeforeの子要素に挿入
+//         before.appendChild(img);
+//     }
+// }
 
 //ブラウザがFIle APIに対応しているか確認※条件式内に判定したいブラウザAPI等の情報を入れることで判定できる
-if (window.File && window.FileReader && window.FileList && window.Blob) {
-    //input[file]にてファイルが選択されたときの処理
-    imgFile.addEventListener('change', loadLocalImg, false);
-} else {
-    imgFile.style.display = 'none'
-    before.innerHTML = 'File APIに対応したブラウザでご確認ください'
-}
-
-
+// if (window.File && window.FileReader && window.FileList && window.Blob) {
+//     //input[file]にてファイルが選択されたときの処理
+//     imgFile.addEventListener('change', loadLocalImg, false);
+// } else {
+//     imgFile.style.display = 'none'
+//     before.innerHTML = 'File APIに対応したブラウザでご確認ください'
+// }
 
 // //画像のパスをsrcに設定
 // image.src = reader.result;
@@ -116,17 +111,17 @@ if (window.File && window.FileReader && window.FileList && window.Blob) {
 // a.href = window.URL.createObjectURL(blob);
 
 //canvasの画像のダウンロード方法
-const canvas = document.getElementById('imgArea');
-const a = document.getElementById('a')
-const ctx2 = canvas.getContext('2d');
+// const canvas = document.getElementById('imgArea');
+// const a = document.getElementById('a')
+// const ctx2 = canvas.getContext('2d');
 
-ctx2.fillStyle = "blue";
-ctx2.fillRect(10, 20, 100, 100);
+// ctx2.fillStyle = "blue";
+// ctx2.fillRect(10, 20, 100, 100);
 
-//blobにはcanvasで作成したオブジェクトが入ってくるので定義の必要なし。※イベントオブジェクトのようなもの
-canvas.toBlob((blob) => {
-    a.href = window.URL.createObjectURL(blob);
-})
+// //blobにはcanvasで作成したオブジェクトが入ってくるので定義の必要なし。※イベントオブジェクトのようなもの
+// canvas.toBlob((blob) => {
+//     a.href = window.URL.createObjectURL(blob);
+// })
 
 //Blobの文字をbase64に変換する方法
 // const str = '小沼　邦明';
@@ -144,5 +139,12 @@ canvas.toBlob((blob) => {
 // const result = window.atob(b64.replace(/^.*,/, ''));
 
 // console.log(result);
+
+//===== FileSaver.jsの使用======//
+//ファイル保存などをクロスブラウザ対応で実装するには大変なので解決策としてFileSaver.jsを使用する
+// FileSaver.jsは、Blob形式のデータをクロスブラウザの実装を行うことができるライブラリ
+
+console.log(navigator.userAgent);
+
 
 
